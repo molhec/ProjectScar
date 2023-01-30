a09d1498c95fa2b254a50dcf9e44ffe26d1bbfbc
hmolinaAmber <molhec_27@hotmail.com>
2023-01-30 15:20:51 -0600

    Initial Commit

---
+++ Source/ProjectScar/ProjectScarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectScarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectScarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectScarGameMode();
+};
+
+
+
