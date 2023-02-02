dddcac9327d0d062b4078521b787439add3129c5
hmolinaAmber <hmolina@amberstudio.com>
2023-02-02 14:01:17 -0600

    Rider Link Stuff

--- Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/RdTextBuffer.cpp.d
+++
@@ -1,4 +0,0 @@
-/Users/molhec/Documents/Unreal\ Projects/ProjectScar/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/RdTextBuffer.cpp.o: \
-  /Users/molhec/Documents/Unreal\ Projects/ProjectScar/Plugins/Developer/RiderLink/Source/RD/src/rd_gen_cpp/src/RdTextBuffer.cpp \
-  /Users/molhec/Documents/Unreal\ Projects/ProjectScar/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/Definitions.h \
-  /Users/molhec/Documents/Unreal\ Projects/ProjectScar/Plugins/Developer/RiderLink/Source/RD/src/rd_gen_cpp/src/RdTextBuffer.h
